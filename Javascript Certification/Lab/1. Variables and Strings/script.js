3476c2cb5e1877c8ed1a1fe273a2fb37290035c6
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2026-04-03 15:48:35 +0700

    JS Workshop "Build a Sentence Maker"

---
+++ Javascript Certification/Lab/1. Variables and Strings/script.js
@@ -0,0 +1,51 @@
+let adjective = "huge";
+let noun = "Dragon";
+let verb = "breathing";
+let place = "Tree House";
+let adjective2 = "rounded";
+let noun2 = "Cheesecake";
+
+const firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "crazy";
+noun = "Flame Knight";
+noun2 = "Hamburger";
+place = "Dungeon";
+adjective2 = "shiny";
+verb = "breathing";
+
+const secondStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("Second story: " + secondStory);
